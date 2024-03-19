09223b7fc79dca48c7e72d7591c025e9b7b5a563
mac <ples.sash@gmail.com>
2024-03-19 15:26:50 +0300

    sout rename

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,6 +1,6 @@
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello!");
     }
 }
     //programm ver0.1.1
