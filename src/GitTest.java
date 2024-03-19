b5343390d97825d32cb04b3a566b5e4ade7c0ed7
mac <ples.sash@gmail.com>
2024-03-19 15:22:52 +0300

    Add sout

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,6 +1,6 @@
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git!", "Welcome");
     }
 }
     //programm ver0.1.1
